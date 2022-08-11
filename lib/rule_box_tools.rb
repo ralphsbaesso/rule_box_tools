04b447cfec803e06b1c363e0e4441bb0be576792
Ralph Baesso <ralphsbaesso@gmail.com>
2022-08-11 14:18:58 -0300

    init

---
+++ lib/rule_box_tools.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rule_box_tools/version"
+
+module RuleBoxTools
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rule_box_tools/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module RuleBoxTools
+  VERSION = "0.1.0"
+end
